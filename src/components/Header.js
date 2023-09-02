3d0db527004dd89dbe6ba4dfda1220cf844443d4
Ochiengsteven <stevenochieng6305@gmail.com>
2023-09-02 12:49:17 +0300

    ADD THE CRYPTO CARDS

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './assets/css/header.css';
+
+export default function Header({ className }) {
+  return (
+    <div className={className}>
+      <h1 className="text-5xl font-extrabold w-full pb-4">CryptoView</h1>
+      <h3 className="text-3xl font-semibold w-full">
+        Research the&nbsp;
+        <br />
+        crypto
+        <br />
+        market
+      </h3>
+    </div>
+  );
+}
+
+// Prop validation
+Header.propTypes = {
+  className: PropTypes.string.isRequired,
+};
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
-/* eslint-disable react/prop-types */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 as uuidv4 } from 'uuid';
 import { fetchAssets, selectAllAssets, selectAssetsStatus } from '../redux/actions/actionsSlice';
+import Header from './Header';
 
 function Home() {
   const dispatch = useDispatch();
   const cryptoData = useSelector(selectAllAssets);
   const assetsStatus = useSelector(selectAssetsStatus);
+
+  // State variables for card display
+  const [displayCount, setDisplayCount] = useState(6); // Initial display count
+  const increment = 4; // Number of cards to add on "View More" click
 
   useEffect(() => {
     if (assetsStatus === 'idle') {
@@ -14,14 +20,37 @@ function Home() {
     }
   }, [assetsStatus, dispatch]);
 
+  // Function to handle "View More" button click
+  const handleViewMoreClick = () => {
+    setDisplayCount(displayCount + increment);
+  };
+
   return (
-    <div>
-      {cryptoData.map((crypto) => (
-        <div className="crypto-card" key={crypto.volume}>
+    <div className="grid grid-cols-2">
+      <Header className="col-span-2 background-image" />
+      {cryptoData.slice(0, displayCount).map((crypto, index) => (
+        <div
+          className="crypto-card"
+          key={uuidv4()}
+          style={{
+            border: 'none',
+            color: 'white',
+            height: '30vh',
+            backgroundColor: index % 2 === 0 ? '#202123' : '#444654',
+          }}
+        >
           <h3>{crypto.pair}</h3>
-          {/* <p>{crypto.data[0].symbol}</p> */}
+          <p>
+            price:&nbsp;
+            {parseFloat(crypto.price).toFixed(2)}
+          </p>
         </div>
       ))}
+      {displayCount < cryptoData.length && (
+        <button type="button" onClick={handleViewMoreClick} className="view-more-button">
+          View More
+        </button>
+      )}
     </div>
   );
 }
